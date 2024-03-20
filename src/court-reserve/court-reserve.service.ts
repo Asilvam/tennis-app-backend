9a3afc531e87b59d4a1c7636fb37fa9b31852c72
alejandrosilvamonsalve <alejmsilvam@falabella.cl>
2024-03-20 20:13:54 -0300

    update version update find to date today

--- src/court-reserve/court-reserve.service.ts
+++ src/court-reserve/court-reserve.service.ts
@@ -2,6 +2,7 @@ import {Injectable, Logger} from '@nestjs/common';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {CourtReserve} from "./entities/court-reserve.entity";
+import { LessThanOrEqual } from 'typeorm';
 
 @Injectable()
 export class CourtReserveService {
@@ -15,7 +16,13 @@ export class CourtReserveService {
 
     async getAllCourtReserves(): Promise<CourtReserve[]> {
         try {
-            return await this.courtReserveRepository.find();
+            const today = new Date();
+            // Query court reserves where dateToPlay is greater than or equal to today's date
+            return await this.courtReserveRepository.find({
+                where: {
+                    dateToPlay: LessThanOrEqual(today),
+                },
+            });
         } catch (error) {
             this.logger.error('Error retrieving court reserves:', error);
             throw error; // Optionally re-throw the error to propagate it
