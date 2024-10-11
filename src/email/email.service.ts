55bc7636708bfdd271e315d1a21b4c6d4f94d166
alejandrosilvamonsalve <alejmsilvam@falabella.cl>
2024-10-11 18:16:54 -0300

    add verify email

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -8,18 +8,28 @@ export class EmailService {
   constructor(private mailerService: MailerService) {}
 
   async sendEmail(sendEmailDto: SendEmailDto) {
-    const { to, subject, text } = sendEmailDto;
+    const { to, subject, html } = sendEmailDto;
 
     try {
       await this.mailerService.sendMail({
         to,
         subject,
-        text, // Make sure `text` is directly passed, or use `html` for rich text emails
+        html, // Make sure `text` is directly passed, or use `html` for rich text emails
       });
       this.logger.log(`Email sent successfully to ${to}`);
     } catch (error) {
       this.logger.error(`Failed to send email to ${to}: ${error.message}`, error.stack);
       throw new Error(`Failed to send email: ${error.message}`);
     }
+  }
+
+  async sendVerificationEmail(email: string, verificationLink: string) {
+    this.logger.log(email, verificationLink);
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Verify your email',
+      html: `<p>Please verify your email by clicking on the link below:</p>
+               <a href="${verificationLink}">Verify Email</a>`,
+    });
   }
 }
